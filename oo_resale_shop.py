2ce6196467ae37402b3e44ed0bd8bbd8b44ff519
Your Name <you@example.com>
2023-02-08 22:09:26 -0500

    Completed computer and resale shop files

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,22 @@
+from computer import *
+
 class ResaleShop:
 
     # What attributes will it need?
-
+    # empty inventory
+    
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self):
+        self.inventory = []
 
     # What methods will you need?
+    def buy(self, computer: Computer):
+        self.inventory.append(computer)
+       
+    def sell(self, sell):
+        self.inventory.remove(sell)
+
+    def inventory(self):
+        print(self.inventory)
+    
